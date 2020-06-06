ff038d939a515b455cf2d24805874ef39073d081
palmerio <palmerio_julio@hotmail.com>
2020-06-06 20:26:32 -0300

    Projeto E-coleta da Rocketseat

---
+++ src/server.js
@@ -0,0 +1,77 @@
+const express = require("express")
+const server = express()
+
+const db = require("./database/db")
+
+server.use(express.urlencoded({ extended: true }))
+server.use(express.static("public"))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+   express: server,
+   noCache: true
+})
+
+server.get("/", (req, res) => {
+   return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+   return res.render("create-point.html")
+})
+
+server.post("/savepoint", (req, res) => {
+   console.log(req.body)
+   const query = `
+      INSERT INTO places (
+         image,
+         name,
+         address,
+         address2,
+         state,
+         city,
+         items
+      ) values (?,?,?,?,?,?,?);
+   `
+   const values = [
+      req.body.image,
+      req.body.name,
+      req.body.address,
+      req.body.address2,
+      req.body.state,
+      req.body.city,
+      req.body.items
+   ]
+
+   function afterInsertData(err) {
+      if (err) {
+         console.log(err)
+         return res.send("Erro no Cadastro!")
+      }
+      console.log("Cadastrado com sucesso")
+      console.log(this)
+
+      return res.render("create-point.html", { saved: true })
+   }
+   db.run(query, values, afterInsertData)
+})
+
+server.get("/search", (req, res) => {
+
+   const search = req.query.search
+
+   if (search == "") {
+      return res.render("search.html", { total: 0 })
+   }
+
+
+   db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+      if (err) {
+         return console.log(err)
+      }
+      const total = rows.length
+      return res.render("search.html", { places: rows, total: total })
+   })
+})
+
+server.listen(3000)
